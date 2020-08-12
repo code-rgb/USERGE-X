6130599d77ae8356e795cefd511a581c8e51b715
code-rgb <62891774+code-rgb@users.noreply.github.com>
2020-08-12 08:58:25 +0530

    Hmmmmm

--- userge/plugins/help.py
+++ userge/plugins/help.py
@@ -427,7 +427,7 @@ if Config.BOT_TOKEN and Config.OWNER_ID:
             if string =="buttonnn":
                 BUTTON_BASE = get_collection("TEMP_BUTTON")  
                 async for data in BUTTON_BASE.find():
-                    button_data = data['msg_data']
+                    button_data = str(data['msg_data'])
                 text, buttons = pb(button_data)
                 
                 results.append(
