0e1a527e6352e2c480c356a4ff0ab1a11e91c0f5
code-rgb <62891774+code-rgb@users.noreply.github.com>
2020-08-14 13:21:39 +0530

    Update callback_test.py

--- userge/plugins/callback_test.py
+++ userge/plugins/callback_test.py
@@ -1,13 +1,10 @@
-#name callback_test.py
 import re 
 from pyrogram import (
     InlineQueryResultArticle, InputTextMessageContent,
     InlineKeyboardMarkup, InlineKeyboardButton,
     Filters, CallbackQuery, InlineQuery, InlineQueryResultPhoto)
-from pyrogram.errors.exceptions.bad_request_400 import MessageNotModified, MessageIdInvalid, UserIsBot, BadRequest, MessageEmpty
-from userge import userge, Message, Config, get_collection
 
-# unnecessary imports IKR
+from userge import userge, Message, Config
 
 if Config.BOT_TOKEN and Config.OWNER_ID:
     if Config.HU_STRING_SESSION:
@@ -15,17 +12,6 @@ if Config.BOT_TOKEN and Config.OWNER_ID:
     else:
         ubot = userge
 
-# def error_call(_, CallbackQuery):
-#     if re.match("notfound", CallbackQuery.data):
-#         return True
-
-# call_create = Filters.create(error_call)
-
-@ubot.on_callback_query(filters=Filters.regex(pattern=r"^right_btn$"))
-async def xyz_callback(CallbackQuery):
-    await CallbackQuery.edit_message_text("processing...")
-    if CallbackQuery.data == 'right_btn':
-        await CallbackQuery.edit_message_text("YOU Pressed Right Button")
-    elif CallbackQuery.data == 'left_btn':
-        await CallbackQuery.edit_message_text("YOU Pressed Left Button")
-    await CallbackQuery.answer("Tell Me if you can see this text", show_alert=True)
+@ubot.on_callback_query(filters=Filters.regex(pattern=r"^btn_press$"))
+async def test_callback(callback_query: CallbackQuery):
+    await callback_query.answer("Button contains: '{}'".format(callback_query.data), show_alert=True)
