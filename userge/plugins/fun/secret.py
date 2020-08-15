5a7bf4e6abd76649a6a81e96e3cc3733e7da6136
code-rgb <62891774+code-rgb@users.noreply.github.com>
2020-08-15 15:30:39 +0530

    NOW UPDATE

--- userge/plugins/fun/secret.py
+++ userge/plugins/fun/secret.py
@@ -11,9 +11,9 @@ if Config.BOT_TOKEN and Config.OWNER_ID:
 @ubot.on_callback_query(filters=Filters.regex(pattern=r"^secret_btn$"))
 async def alive_callback(_, callback_query: CallbackQuery):
     async for data in SECRET_MSG.find():
-        user_id = int(data['user_id'])
+        receiver = int(data['user_id'])
         msg = data['msg']
-    if callback_query.from_user.id == (Config.OWNER_ID or user_id):
+    if callback_query.from_user.id == Config.OWNER_ID or callback_query.from_user.id == receiver:
         await callback_query.answer(msg, show_alert=True)
     else:
         await callback_query.answer("This Message is Confidential 👽", show_alert=True)
