ed649ba40766ea06494fc0a661b6f09cf12e59dd
code-rgb <62891774+code-rgb@users.noreply.github.com>
2020-08-18 23:25:35 +0530

    auto delete alive message
     after 25 sec

--- userge/plugins/tools/alive.py
+++ userge/plugins/tools/alive.py
@@ -1,15 +1,22 @@
 from pyrogram import Filters, CallbackQuery
 from userge import userge, Message, Config
 from userge.core.ext import RawClient
+import asyncio
+
+
 @userge.on_cmd("alive", about={
     'header': "Just For Fun"}, allow_channels=False)
 async def alive_inline(message: Message):
     bot = await userge.bot.get_me()
     x = await userge.get_inline_bot_results(bot.username, "alive")
-    await userge.send_inline_bot_result(chat_id=message.chat.id,
+    
+    y = await userge.send_inline_bot_result(chat_id=message.chat.id,
                                         query_id=x.query_id,
                                         result_id=x.results[1].id)
     await message.delete()
+    await asyncio.sleep(25)
+    await userge.delete_messages(message.chat.id, y.updates[2].message.id)
+
 
 if Config.BOT_TOKEN and Config.OWNER_ID:
     if Config.HU_STRING_SESSION:
@@ -17,10 +24,11 @@ if Config.BOT_TOKEN and Config.OWNER_ID:
     else:
         ubot = userge
 
+
     @ubot.on_callback_query(filters=Filters.regex(pattern=r"^settings_btn$"))
     async def alive_cb(_, callback_query: CallbackQuery):
         alive_settings=f"""
-   🕔 Uptime : {userge.uptime}
+    🕔 Uptime : {userge.uptime}
 
 • 👥 𝗦𝘂𝗱𝗼 : {_parse_arg(Config.SUDO_ENABLED)}
 • 🚨 𝗔𝗻𝘁𝗶𝘀𝗽𝗮𝗺 : {_parse_arg(Config.ANTISPAM_SENTRY)}
@@ -28,6 +36,7 @@ if Config.BOT_TOKEN and Config.OWNER_ID:
 • ➕ 𝗘𝘅𝘁𝗿𝗮 𝗣𝗹𝘂𝗴𝗶𝗻𝘀 : {_parse_arg(Config.LOAD_UNOFFICIAL_PLUGINS)}
 """
         await callback_query.answer(alive_settings, show_alert=True)
+
 
 def _parse_arg(arg: bool) -> str:
     return " ✅ 𝙴𝚗𝚊𝚋𝚕𝚎𝚍" if arg else " ❌ 𝙳𝚒𝚜𝚊𝚋𝚕𝚎𝚍"     
