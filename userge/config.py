ad5151c843fb01addfa397cef33990a66d9d1135
code-rgb <62891774+code-rgb@users.noreply.github.com>
2020-08-25 20:06:18 +0530

    Hotfix

--- userge/config.py
+++ userge/config.py
@@ -95,15 +95,10 @@ if Config.HEROKU_API_KEY:
 def get_version() -> str:
     """ get userge version """
     ver = f"{versions.__major__}.{versions.__minor__}.{versions.__micro__}"
-    if "/usergeteam/userge" in Config.UPSTREAM_REPO.lower():
-        diff = list(_REPO.iter_commits('v0.1.6..HEAD'))  # temp solution
+    try:
+        diff = list(_REPO.iter_commits(f'{Config.UPSTREAM_REMOTE}/master..HEAD'))
         if diff:
-            return f"{ver}-patch.{len(diff)}"
-    else:
-        try:
-            diff = list(_REPO.iter_commits(f'{Config.UPSTREAM_REMOTE}/master..HEAD'))
-            if diff:
-                return f"{ver}-custom.{len(diff)}"
-        except:
-            error = "Update repo to get version."
-            return error
+            return f"{ver}-[X].{len(diff)}"
+    except:
+        error = "Update repo to get version."
+        return error
