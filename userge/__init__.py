ea5b953172f051dc8283f4a2e2528b7442dab3ed
code-rgb <62891774+code-rgb@users.noreply.github.com>
2020-09-10 17:23:06 +0530

    Ignore

--- userge/__init__.py
+++ userge/__init__.py
@@ -12,4 +12,5 @@ from userge.logger import logging  # noqa
 from userge.config import Config, get_version  # noqa
 from userge.core import (  # noqa
     Userge, filters, Message, get_collection, pool)
+from userge.functions import deEmojify, media_to_image
 userge = Userge()  # userge is the client name
