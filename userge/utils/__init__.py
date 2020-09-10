60bc867ff4dfe85be1bc66c787b1f2ba3b7f22e3
code-rgb <62891774+code-rgb@users.noreply.github.com>
2020-09-10 17:40:29 +0530

    moved functions.py to utils
    >> old way -
    from userge import deEmojify, media_to_image
    
    >> new way -
    from userge.utils import deEmojify, media_to_image

--- userge/utils/__init__.py
+++ userge/utils/__init__.py
@@ -10,11 +10,11 @@
 
 from .progress import progress  # noqa
 from .sys_tools import SafeDict, get_import_path  # noqa
-from .tools import (demojify,  # noqa
-                    get_file_id_and_ref,
+from .tools import (get_file_id_and_ref,
                     humanbytes,
                     time_formatter,
                     post_to_telegraph,
                     runcmd,
                     take_screen_shot,
                     parse_buttons)
+from .functions import deEmojify, media_to_image
