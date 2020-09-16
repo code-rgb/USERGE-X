c20c0bfbc72313f81be62a08d186fe4e7f4ac153
code-rgb <62891774+code-rgb@users.noreply.github.com>
2020-09-17 00:21:42 +0530

    Update config.py

--- userge/config.py
+++ userge/config.py
@@ -89,15 +89,14 @@ def get_version() -> str:
     ver = f"{versions.__major__}.{versions.__minor__}.{versions.__micro__}"
     try:
         if "/code-rgb/userge-x" in Config.UPSTREAM_REPO.lower():
-            diff = list(_REPO.iter_commits("v0.2.2..HEAD"))
+            diff = list(_REPO.iter_commits(f'v{ver}..HEAD'))
             if diff:
                 return f"{ver}-Mystique.{len(diff)}"
         else:
-            diff = list(_REPO.iter_commits(f'{Config.UPSTREAM_REMOTE}/alpha..HEAD'))
+            diff = list(_REPO.iter_commits(f'{Config.UPSTREAM_REMOTE}/master..HEAD'))
             if diff:
                 return f"{ver}-fork-[X].{len(diff)}"
     except:
-        error = " Undefined"
+        error = " Idk What the F* is wrong here"
         return error
     return ver
-        
