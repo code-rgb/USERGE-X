27f83eb817bbe6039bf9cf3da487d7eb55c4c991
code-rgb <62891774+code-rgb@users.noreply.github.com>
2020-09-20 18:07:29 +0530

    Improved inline help

--- userge/plugins/bot/inline_help.py
+++ userge/plugins/bot/inline_help.py
@@ -8,27 +8,34 @@ from pyrogram.types import (
 from pyrogram import filters
 
 
+HELP_BUTTONS = None
+
+
+COMMANDS = {
+            "secret" : { 'help_txt' : 'Send a secret message\n\n`secret @username [text]`', 'i_q' : 'secret @DeletedUser420 This is a secret message'},
+            "alive" : { 'help_txt' : 'Alive Command for USERGE-X', 'i_q' : 'alive'},
+            "opinion" : { 'help_txt' : 'Ask for opinion via inline\n\n`op Your Question`', 'i_q' : 'op Are Cats Cute ?'},
+            "repo" : { 'help_txt' : 'Your USERGE-X Github repo', 'i_q' : 'repo'},
+            "gapps" : { 'help_txt' : 'Lastest arm64 Gapps', 'i_q' : 'gapps'},
+            "ofox" : { 'help_txt' : 'Lastest Ofox Recovery\n\n`ofox <device codename>`', 'i_q' : 'ofox whyred'},
+            "rick" : { 'help_txt' : 'Useless Rick Roll', 'i_q' : 'rick'},
+            "help" : { 'help_txt' : 'Help For All Bot plugins', 'i_q' : ''},
+            "stylish" : { 'help_txt' : 'Write it in Style\n\n`stylish [text]`', 'i_q' : 'stylish USERGE-X'}
+            }
+
+
 if Config.BOT_TOKEN and Config.OWNER_ID:
     if Config.HU_STRING_SESSION:
         ubot = userge.bot
     else:
         ubot = userge
+   
+   
+    if not HELP_BUTTONS:
+        HELP_BUTTONS = help_btn_generator()
 
-    HELP_BUTTONS =  [
-                [InlineKeyboardButton("Secret", callback_data="help_secret"),
-                InlineKeyboardButton("Alive", callback_data="help_alive"),
-                InlineKeyboardButton("Opinion", callback_data="help_opinion")],
+    BACK_BTN = InlineKeyboardButton("◀️ BACK", callback_data="backbtn_ihelp")
 
-                [InlineKeyboardButton("Ofox", callback_data="help_ofox"),
-                InlineKeyboardButton("Gapps", callback_data="help_gapps"),
-                InlineKeyboardButton("Stylish", callback_data="help_stylish")],
-
-                [InlineKeyboardButton("Repo", callback_data="help_repo"),
-                InlineKeyboardButton("Rick", callback_data="help_rick"),
-                InlineKeyboardButton("Help", callback_data="help_help")]
-    ]
-
-    BACK_BTN = InlineKeyboardButton("◀️ BACK", callback_data="help_backbtn")
 
     @ubot.on_message(filters.private & filters.command("inline"))
     async def inline_help(_, message: Message):
@@ -37,7 +44,9 @@ if Config.BOT_TOKEN and Config.OWNER_ID:
             text="<u><b>INLINE COMMANDS</b></u>",
             reply_markup=InlineKeyboardMarkup(HELP_BUTTONS)
         )
-    @ubot.on_callback_query(filters.regex(pattern=r"^help_backbtn$"))
+
+
+    @ubot.on_callback_query(filters.regex(pattern=r"^backbtn_ihelp$"))
     async def back_btn(_, c_q: CallbackQuery): 
         await c_q.edit_message_text(
             text="<u><b>INLINE COMMANDS</b></u>",
@@ -45,68 +54,28 @@ if Config.BOT_TOKEN and Config.OWNER_ID:
         )
 
 
-    @ubot.on_callback_query(filters.regex(pattern=r"^help_secret$"))
-    async def gfmnfmfft(_, c_q: CallbackQuery): 
-        buttons = [[BACK_BTN, InlineKeyboardButton("📕 Example", switch_inline_query_current_chat="secret @DeletedUser420 *Random GOT spolier*")]]
-        msg = "Send a secret message\n\n`secret @username [text]`"
+    @ubot.on_callback_query(filters.regex(pattern=r"^ihelp_([a-zA-Z]+)$"))
+    async def help_query(_, c_q: CallbackQuery): 
+        command_name = c_q.matches[0].group(1)
+        msg = COMMANDS[command_name]['help_txt']
+        switch_i_q = COMMANDS[command_name]['i_q']
+        buttons = [[BACK_BTN, InlineKeyboardButton("📕 Example", switch_inline_query_current_chat=switch_i_q)]]
         await c_q.edit_message_text(
                 msg,
                 reply_markup=InlineKeyboardMarkup(buttons)
         )
-    @ubot.on_callback_query(filters.regex(pattern=r"^help_alive$"))
-    async def gfmngn(_, c_q: CallbackQuery): 
-        buttons = [[BACK_BTN, InlineKeyboardButton("📕 Example", switch_inline_query_current_chat="alive")]]
-        msg = "Alive Command for USERGE-X"
-        await c_q.edit_message_text(
-                msg,
-                reply_markup=InlineKeyboardMarkup(buttons)
+
+
+def help_btn_generator():
+    btn = []
+    b = []
+    for cmd in list(COMMANDS.keys()):
+        name = cmd.capitalize()
+        call_back = f"ihelp_{cmd}"
+        b.append(
+            InlineKeyboardButton(name, callback_data=call_back)
         )
-    @ubot.on_callback_query(filters.regex(pattern=r"^help_opinion$"))
-    async def mfhmgtfym(_, c_q: CallbackQuery): 
-        buttons = [[BACK_BTN, InlineKeyboardButton("📕 Example", switch_inline_query_current_chat="op Are Cats Cute?")]]
-        msg = "Ask for opinion via inline\n\n`op Your Question`"
-        await c_q.edit_message_text(
-                msg,
-                reply_markup=InlineKeyboardMarkup(buttons)
-        )
-    @ubot.on_callback_query(filters.regex(pattern=r"^help_repo$"))
-    async def hgcmghmg(_, c_q: CallbackQuery): 
-        buttons = [[BACK_BTN, InlineKeyboardButton("📕 Example", switch_inline_query_current_chat="repo")]]
-        msg = "Your USERGE-X github repo"
-        await c_q.edit_message_text(
-                msg,
-                reply_markup=InlineKeyboardMarkup(buttons)
-        )
-    @ubot.on_callback_query(filters.regex(pattern=r"^help_gapps$"))
-    async def dsbsbas(_, c_q: CallbackQuery): 
-        buttons = [[BACK_BTN, InlineKeyboardButton("📕 Example", switch_inline_query_current_chat="gapps")]]
-        msg = "Get Lastest arm64 Gapps"
-        await c_q.edit_message_text(
-                msg,
-                reply_markup=InlineKeyboardMarkup(buttons)
-        )
-    @ubot.on_callback_query(filters.regex(pattern=r"^help_ofox$"))
-    async def sdvdvsv(_, c_q: CallbackQuery): 
-        buttons = [[BACK_BTN, InlineKeyboardButton("📕 Example", switch_inline_query_current_chat="ofox begonia")]]
-        msg = "Get Latest Ofox recovery for your device\n\n`ofox <device codename>`"
-        await c_q.edit_message_text(
-                msg,
-                reply_markup=InlineKeyboardMarkup(buttons)
-        )
-    @ubot.on_callback_query(filters.regex(pattern=r"^help_rick$"))
-    async def avas(_, c_q: CallbackQuery): 
-        buttons = [[BACK_BTN, InlineKeyboardButton("📕 Example", switch_inline_query_current_chat="rick")]]
-        msg = "Rick Roll"
-        await c_q.edit_message_text(
-                msg,
-                reply_markup=InlineKeyboardMarkup(buttons)
-        )
-    @ubot.on_callback_query(filters.regex(pattern=r"^help_help$"))
-    async def asca(_, c_q: CallbackQuery): 
-        buttons = [[BACK_BTN, InlineKeyboardButton("📕 Example", switch_inline_query_current_chat="")]]
-        msg = "Inline Plugin Help"
-        await c_q.edit_message_text(
-                msg,
-                reply_markup=InlineKeyboardMarkup(buttons)
-        )
-    
+        if len(b) == 3:
+            btn.append(b)
+            b = []
+    return btn
