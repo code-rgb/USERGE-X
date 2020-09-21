cc2e7d699bd9f6010f21dddfd1cd4d781112e89b
Leorio Paradinight <62891774+code-rgb@users.noreply.github.com>
2020-09-21 12:30:54 +0530

    Update inline_help.py

--- userge/plugins/bot/inline_help.py
+++ userge/plugins/bot/inline_help.py
@@ -1,5 +1,6 @@
 # Copyright (C) 2020 BY - GitHub.com/code-rgb [TG - @deleteduser420]
 # All rights reserved.
+
 """Module that handles Inline Help"""
 
 from userge import userge, Message, Config
@@ -9,50 +10,18 @@ from pyrogram import filters
 
 
 HELP_BUTTONS = None
-
 PRIV_USERS = list(Config.SUDO_USERS)
 PRIV_USERS.append(Config.OWNER_ID)
 
 
-rick_roll = """
-We're no strangers to love
-You know the rules and so do I
-A full commitment's what I'm thinking of
-You wouldn't get this from any other guy
-
-I just wanna tell you how I'm feeling
-Gotta make you understand
-
-Never gonna give you up
-Never gonna let you down
-Never gonna run around and desert you
-Never gonna make you cry
-Never gonna say goodbye
-Never gonna tell a lie and hurt you
-
-We've known each other for so long
-Your heart's been aching, but
-You're too shy to say it
-Inside, we both know what's been going on
-We know the game and we're gonna play it
-
-And if you ask me how I'm feeling
-Don't tell me you're too blind to see
-
-Never gonna give you up
-Never gonna let you down
-...
-"""
-
-
 COMMANDS = {
             "secret" : { 'help_txt' : '**Send a secret message to a user**\n (only the entered user and you can view  the message)\n\n`secret @username [text]`', 'i_q' : 'secret @DeletedUser420 This is a secret message'},
-            "alive" : { 'help_txt' : '**Alive Command for USERGE-X**', 'i_q' : 'alive'},
-            "opinion" : { 'help_txt' : '**Ask for opinion via inline**\n\n`op [Question or Statement]`', 'i_q' : 'op Are Cats Cute ?'},
-            "repo" : { 'help_txt' : '**Your USERGE-X Github repo**', 'i_q' : 'repo'},
-            "gapps" : { 'help_txt' : '**Lastest arm64 Gapps**\n\n`Choose from Niksgapps, Opengapps and Flamegapps`', 'i_q' : 'gapps'},
-            "ofox" : { 'help_txt' : '**Lastest Ofox Recovery for supported device, Powered By offcial Ofox API**\n\n`ofox <device codename>`', 'i_q' : 'ofox whyred'},
-            "rick" : { 'help_txt' : f'**Useless Rick Roll**\n\n{rick_roll}\n`rick`', 'i_q' : 'rick'},
+            "alive" : { 'help_txt' : '**Alive Command for USERGE-X**\n\nThis You can view Uptime, Setting and Versions of your bot and when you change settings they are updayted in Real-time UwU', 'i_q' : 'alive'},
+            "opinion" : { 'help_txt' : '**Ask for opinion via inline**\n\nYou can now send multple opinion messages at once\n**Note: **All button data is cleared as soon as you restart or update bot\n`op [Question or Statement]`', 'i_q' : 'op Are Cats Cute ?'},
+            "repo" : { 'help_txt' : '**Your USERGE-X Github repo**\n\nwith direct deploy button', 'i_q' : 'repo'},
+            "gapps" : { 'help_txt' : '**Lastest arm64 Gapps for <u>Android 10 Only !</u>**\n\n`Choose from Niksgapps, Opengapps and Flamegapps`', 'i_q' : 'gapps'},
+            "ofox" : { 'help_txt' : '**Lastest Ofox Recovery for supported device, Powered By offcial Ofox API v2**\n\n`ofox <device codename>`', 'i_q' : 'ofox whyred'},
+            "rick" : { 'help_txt' : '**Useless Rick Roll**\n\nBasically a wierd looking button with rickroll link xD\n`rick`', 'i_q' : 'rick'},
             "help" : { 'help_txt' : '**Help For All Userbot plugins**\n\n**Note:** `You can load and unload a plugin`', 'i_q' : ''},
             "stylish" : { 'help_txt' : '**Write it in Style**\n\nplugin to decorate text with unicode fonts.\n`stylish [text]`', 'i_q' : 'stylish USERGE-X'}
             }
@@ -87,7 +56,7 @@ if Config.BOT_TOKEN and Config.OWNER_ID:
 
     BACK_BTN = InlineKeyboardButton(" ◀️  Back ", callback_data="backbtn_ihelp")
 
-    inline_help_txt ="<u><b>INLINE COMMANDS</b></u>\n\nHere is a list of all available inline commands.\nChoose a command and click **📕  EXAMPLE** to know the use."
+    inline_help_txt ="◼️ <u><b>INLINE COMMANDS</b></u>\n\nHere is a list of all available inline commands.\nChoose a command and for usage see: [ **📕  EXAMPLE** ]"
             
 
     @ubot.on_message(filters.user(PRIV_USERS) & filters.private & (filters.command("inline") | filters.regex(pattern=r"^/start inline$")))
