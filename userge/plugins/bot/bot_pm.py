ec46c229d8921c6f0ece8841756d5c3598718935
code-rgb <62891774+code-rgb@users.noreply.github.com>
2020-09-23 12:46:20 +0530

    testing

--- userge/plugins/bot/bot_pm.py
+++ userge/plugins/bot/bot_pm.py
@@ -18,7 +18,7 @@ BOT_BAN = get_collection("BOT_BAN")
 BOT_START = get_collection("BOT_START")
 LOGO_ID, LOGO_REF = None, None
 _CHAT, _MSG_ID = None, None
-_DEFAULT = "https://t.me/useless_x/2"
+_DEFAULT = "https://t.me/useless_x/4"
 
 # refresh file id and file reference from TG server
 
