a51f818587c78a9175b895009d170cf8d8c0c6bc
Leorio Paradinight <62891774+code-rgb@users.noreply.github.com>
2020-10-03 12:32:26 +0530

    added gif support in redditdl

--- userge/plugins/misc/redditdl.py
+++ userge/plugins/misc/redditdl.py
@@ -24,7 +24,7 @@ async def reddit_fetch(message: Message):
     """Random reddit post"""
     reply = message.reply_to_message
     reply_id = reply.message_id if reply else None
-    sub_r = message.input_str 
+    sub_r = message.input_str
     subreddit_api = f"{API}/{sub_r}" if sub_r else API
     try:
         cn = requests.get(subreddit_api)
@@ -36,12 +36,12 @@ async def reddit_fetch(message: Message):
         code_message = r['message']
         return await CHANNEL.log(f"*Error Code: {code}*\n`{code_message}`")
     else:
-        if not 'url' in r:
+        if 'url' not in r:
             return await message.err('Coudn\'t Find a post with Image, Please Try Again', del_in=3)
         postlink = r['postLink']
         subreddit = r['subreddit']
         title = r['title']
-        image = r['url']
+        media_url = r['url']
         author = r['author']
         upvote = r['ups']
         captionx = f"<b>{title}</b>\n"
@@ -51,22 +51,27 @@ async def reddit_fetch(message: Message):
             captionx += "⚠️ Post marked as SPOILER\n"
         if r['nsfw']:
             captionx += "🔞 Post marked Adult \n"
-    
-    if message.client.is_bot:
-        buttons = [[InlineKeyboardButton(text=f"Source: r/{subreddit}", url=postlink)]] 
-        await userge.bot.send_photo(
+
+        if message.client.is_bot:
+            buttons = InlineKeyboardMarkup([[InlineKeyboardButton(text=f"Source: r/{subreddit}", url=postlink)]])
+        else:
+            await message.delete()
+            captionx += f"Source: [r/{subreddit}]({postlink})"
+            buttons = None
+
+        if media_url.endswith(".gif"):
+            await message.client.send_animation(
             chat_id=message.chat.id,
-            photo=image,
+            animation=media_url,
             caption=captionx,
             reply_to_message_id=reply_id,
-            reply_markup=InlineKeyboardMarkup(buttons)
-        )
-    else:
-        await message.delete()
-        captionx += f"Source: [r/{subreddit}]({postlink})"
-        await userge.send_photo(
-            chat_id=message.chat.id,
-            photo=image,
-            caption=captionx,
-            reply_to_message_id=reply_id,
-        )
+            reply_markup=buttons
+            )
+        else:
+            await message.client.send_photo(
+                chat_id=message.chat.id,
+                photo=media_url,
+                caption=captionx,
+                reply_to_message_id=reply_id,
+                reply_markup=buttons
+            )
