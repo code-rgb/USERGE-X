99fe16a22d239e0deca282667ad0c924710e431e
code-rgb <62891774+code-rgb@users.noreply.github.com>
2020-10-14 10:09:18 +0530

    lets see

--- userge/plugins/bot/utube_inline.py
+++ userge/plugins/bot/utube_inline.py
@@ -1,12 +1,16 @@
 
-
+import os
 import datetime
-import youtube_dl as ytdl
+import youtube_dl
 from pyrogram.types import InlineKeyboardButton, CallbackQuery
 from pyrogram import filters
-from userge import userge, Config
+from userge import userge, Config, pool
+from ..misc.upload import upload
+from time import time
 
-# LOGGER = userge.getLogger(__name__)
+
+LOGGER = userge.getLogger(__name__)
+
 
 def get_ytthumb(thumb_array):
     thumb_link = (thumb_array.pop())['url']
@@ -33,13 +37,13 @@ def ytdl_btn_generator(array, code):
 
 
 def date_formatter(date_):
-    if len(date_) != 8:
+    if len(date_) != 8: # TODO change it according to the input
         return date_
     year, day, month = date_[:4], date_[4:6], date_[6:]
     x = datetime.datetime(int(year), int(month), int(day))
     return str(x.strftime('%d-%b-%Y'))
 
-
+"""
 if Config.BOT_TOKEN and Config.OWNER_ID:
     if Config.HU_STRING_SESSION:
         ubot = userge.bot
@@ -49,15 +53,67 @@ if Config.BOT_TOKEN and Config.OWNER_ID:
        
     @ubot.on_callback_query(filters.regex(pattern=r"^ytdl(\S+)\|(\d+)$"))
     async def ytdl_callback(_, c_q: CallbackQuery):
+        startTime = time()
         u_id = c_q.from_user.id
         if not (u_id == Config.OWNER_ID or u_id in Config.SUDO_USERS):
             return await c_q.answer("𝘿𝙚𝙥𝙡𝙤𝙮 𝙮𝙤𝙪𝙧 𝙤𝙬𝙣 𝙐𝙎𝙀𝙍𝙂𝙀-𝙓", show_alert=True)
-        await c_q.answer("Message Will be Edited Shortly", show_alert=True)
-        yt_code = c_q.matches[0].group(1)
         choice_id = c_q.matches[0].group(2)
+        await c_q.answer(f"Message Will be Edited Shortly\n\nDownloading...\nFormat Code : {choice_id}", show_alert=True)
+        yt_code = c_q.matches[0].group(1)
+        yt_url = f"https://www.youtube.com/watch?v={yt_code}"
+        upload_msg = await userge.send_message(
+            Config.LOG_CHANNEL_ID,
+            "Uploading..."
+        )
+        retcode = await _tubeDl(
+            yt_url,
+            starttime,
+            choice_id
+            )
+        if retcode == 0:
+            _fpath = ''
+            for _path in glob.glob(os.path.join(Config.DOWN_PATH, str(startTime), '*')):
+                if not _path.lower().endswith((".jpg", ".png", ".webp")):
+                    _fpath = _path
+            if not _fpath:
+                await upload_msg.err("nothing found !")
+                return
+            await upload(upload_msg, Path(_fpath))
+        else:
+            return await upload_msg.edit(str(retcode))        
+
+
+
+
         await c_q.edit_message_caption(
-            caption=f"Youtube Link : https://www.youtube.com/watch?v={yt_code}\n\nFormat Code : {choice_id}",
+            caption=f"Youtube Link : \n\nFormat Code : {choice_id}",
             reply_markup=None
         )
 
 
+
+
+
+@pool.run_in_thread
+def _tubeDl(url: list, starttime, uid):
+    ydl_opts = {
+    'outtmpl': os.path.join(Config.DOWN_PATH, str(starttime),
+                                     '%(title)s-%(format)s.%(ext)s'),
+    'logger': LOGGER,
+    'format': str(uid),
+    'writethumbnail': True,
+    'prefer_ffmpeg': True,
+    'postprocessors': [
+        {'key': 'FFmpegMetadata'}]
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        x = ydl.download([url])
+    return x
+        
+
+xa = await _tubeDl(
+    "http://www.youtube.com/watch?v=gl1aHhXnN1k",
+    "399"
+)
+print(xa)
+"""
