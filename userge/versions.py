9799930336cd49c1fb8cb4aaa2eb97289562a219
Leorio Paradinight <62891774+code-rgb@users.noreply.github.com>
2020-10-18 20:55:26 +0530

    Update versions.py

--- userge/versions.py
+++ userge/versions.py
@@ -1,21 +1,11 @@
-# pylint: disable=missing-module-docstring
-#
-# Copyright (C) 2020 by UsergeTeam@Github, < https://github.com/UsergeTeam >.
-#
-# This file is part of < https://github.com/UsergeTeam/Userge > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/uaudith/Userge/blob/master/LICENSE >
-#
-# All rights reserved.
-
 from sys import version_info
 
 from pyrogram import __version__ as __pyro_version__  # noqa
 
 __major__ = 0
 __minor__ = 2
-__micro__ = 4
+__micro__ = 5
 
 __python_version__ = f"{version_info[0]}.{version_info[1]}.{version_info[2]}"
-__license__ = "[GNU GPL v3.0](https://github.com/UsergeTeam/Userge/blob/master/LICENSE)"
-__copyright__ = "[UsergeTeam](https://github.com/UsergeTeam)"
+__license__ = "[GNU GPL v3.0](https://github.com/code-rgb/userge-x/blob/alpha/LICENSE)"
+__copyright__ = "[USERGE-X](https://github.com/code-rgb/userge-x)"
