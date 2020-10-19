f02e60626a346936e8e49e99c2d43918a6a265fa
Leorio Paradinight <62891774+code-rgb@users.noreply.github.com>
2020-10-19 21:17:35 +0530

    Added yaml

--- userge/plugins/tools/json.py
+++ userge/plugins/tools/json.py
@@ -1,11 +1,7 @@
-# Copyright (C) 2020 by UsergeTeam@Github, < https://github.com/UsergeTeam >.
-#
-# This file is part of < https://github.com/UsergeTeam/Userge > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/uaudith/Userge/blob/master/LICENSE >
-#
-# All rights reserved.
 
+import re
+import json
+import yaml
 from userge import userge, Message
 
 
@@ -15,3 +11,20 @@ from userge import userge, Message
 async def jsonify(message: Message):
     msg = str(message.reply_to_message) if message.reply_to_message else str(message)
     await message.edit_or_send_as_file(text=msg, filename="json.txt", caption="Too Large")
+
+
+@userge.on_cmd("yaml", about={
+    'header': "message object to yaml",
+    'usage': "reply {tr}yaml to any message"})
+async def yamlify(message: Message):
+    """get yaml"""
+    msg = str(message.reply_to_message) if message.reply_to_message else str(message)
+    yaml_ify = yaml.dump(json.loads(msg), allow_unicode=True)
+    regex = r"(\s+|)(?:- _:|_:)[\s]"
+    result = re.sub(regex, " ", yaml_ify, re.MULTILINE)
+    if result:
+        await message.edit_or_send_as_file(
+            text=f"```{result[1:]}```",
+            filename="yaml.txt",
+            caption="Too Large"
+        )
