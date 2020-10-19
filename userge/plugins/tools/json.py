683b76c2fff1dc3770f09a3b75034abfce95827a
Leorio Paradinight <62891774+code-rgb@users.noreply.github.com>
2020-10-20 00:05:32 +0530

    Update json.py

--- userge/plugins/tools/json.py
+++ userge/plugins/tools/json.py
@@ -1,25 +1,42 @@
+"""Json / Yaml"""
 
-import re
+# by - @DeletedUser420
+
 import json
+import re
+import sys
+
 import yaml
-from userge import userge, Message
+from userge import Message, userge
 
 
 @userge.on_cmd("json", about={
     'header': "message object to json",
     'usage': "reply {tr}json to any message"})
 async def jsonify(message: Message):
-    msg = str(message.reply_to_message) if message.reply_to_message else str(message)
-    await message.edit_or_send_as_file(text=msg, filename="json.txt", caption="Too Large")
+    """Json-ify"""
+    if message.reply_to_message:
+        msg = str(message.reply_to_message)
+    else:
+        msg = str(message)
+    await message.edit_or_send_as_file(
+        text=msg,
+        filename="json.txt",
+        caption="Too Large"
+    )
 
 
 @userge.on_cmd("yaml", about={
     'header': "message object to yaml",
     'usage': "reply {tr}yaml to any message"})
 async def yamlify(message: Message):
-    """get yaml"""
-    msg = str(message.reply_to_message) if message.reply_to_message else str(message)
-    yaml_ify = yaml.dump(json.loads(msg), allow_unicode=True)
+    """yaml-ify"""
+    if message.reply_to_message:
+        msg = str(message.reply_to_message)
+    else:
+        msg = str(message)
+    json_file = json.loads(msg)
+    yaml_ify = yaml.dump(convert(json_file), allow_unicode=True)
     regex = r"(\s+|)(?:- _:|_:)[\s]"
     result = re.sub(regex, " ", yaml_ify, re.MULTILINE)
     if result:
@@ -28,3 +45,17 @@ async def yamlify(message: Message):
             filename="yaml.txt",
             caption="Too Large"
         )
+
+
+def convert(obj):
+    if isinstance(obj, bool):
+        return bool_emoji(obj)
+    if isinstance(obj, (list, tuple)):
+        return [convert(item) for item in obj]
+    if isinstance(obj, dict):
+        return {convert(key): convert(value) for key, value in obj.items()}
+    return obj
+
+
+def bool_emoji(choice: bool) -> str:
+    return "✔️" if choice else "✖️"
