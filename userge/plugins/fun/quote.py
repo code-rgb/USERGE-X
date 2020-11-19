b3bb6d1aec023c2b557f99db0ea45352ae1c1ccf
Leorio Paradinight <62891774+code-rgb@users.noreply.github.com>
2020-11-19 18:23:35 +0530

    Added multi message support in quote
    Note !!
     .quote renamed to .q

--- userge/plugins/fun/quote.py
+++ userge/plugins/fun/quote.py
@@ -1,32 +1,50 @@
-# Copyright (C) 2020 by UsergeTeam@Github, < https://github.com/UsergeTeam >.
-#
-# This file is part of < https://github.com/UsergeTeam/Userge > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/uaudith/Userge/blob/master/LICENSE >
-#
-# All rights reserved.
+
 
 import asyncio
 
-from pyrogram.errors.exceptions.bad_request_400 import YouBlockedUser
+from pyrogram.errors import YouBlockedUser
 
 from userge import Message, userge
 
 
 @userge.on_cmd(
-    "quote",
-    about={"header": "Quote a message", "usage": "{tr}quote [text or reply to msg]"},
+    "q",
+    about={
+    "header": "Quote a message", "usage": "{tr}q [text or reply to msg]",
+    "flags": {"-l": "limit, for multiple messages"},
+    "usage": "Reply {tr}q -l[message limit]",
+    "examples": [
+        "{tr}q"
+        "{tr}q -l3"
+    ],
+    },
     allow_via_bot=False,
+    del_pre=True
 )
 async def quotecmd(message: Message):
     """quotecmd"""
-    asyncio.get_event_loop().create_task(message.delete())
-    args = message.input_str
+    quote_list = []
     replied = message.reply_to_message
+    if replied and 'l' in message.flags and not message.filtered_input_str:
+        args = ""
+        limit = message.flags.get('l', 1)
+        if limit.isdigit():
+            limit = int(limit)
+        else:
+            return await message.err('give valid no. of message to quote', del_in=3)
+        num = min(limit, 20)
+        async for msg in userge.iter_history(message.chat.id, limit=num, offset_id=replied.message_id, reverse=True):
+            if msg.message_id != message.message_id:
+                quote_list.append(msg.message_id)
+    else:
+        args = message.input_str
+        quote_list.append(replied.message_id)
+    asyncio.get_event_loop().create_task(message.delete())
+
     async with userge.conversation("QuotLyBot") as conv:
         try:
-            if replied and not args:
-                await conv.forward_message(replied)
+            if quote_list and not args:
+                await userge.forward_messages("QuotLyBot", message.chat.id, quote_list)
             else:
                 if not args:
                     await message.err("input not found!")
@@ -36,13 +54,22 @@ async def quotecmd(message: Message):
             await message.edit("first **unblock** @QuotLyBot")
             return
         quote = await conv.get_response(mark_read=True)
-        if not quote.sticker:
+        if not (quote.sticker or quote.document):
             await message.err("something went wrong!")
         else:
             message_id = replied.message_id if replied else None
-            await userge.send_sticker(
-                chat_id=message.chat.id,
-                sticker=quote.sticker.file_id,
-                file_ref=quote.sticker.file_ref,
-                reply_to_message_id=message_id,
-            )
+            if quote.sticker:
+                await userge.send_sticker(
+                    chat_id=message.chat.id,
+                    sticker=quote.sticker.file_id,
+                    file_ref=quote.sticker.file_ref,
+                    reply_to_message_id=message_id,
+                )
+            else:
+                await userge.send_document(
+                    chat_id=message.chat.id,
+                    document=quote.document.file_id,
+                    file_ref=quote.document.file_ref,
+                    reply_to_message_id=message_id,
+                )
+    
