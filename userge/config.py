fc0c61970b5c47cde35c374cdb6b24cc8212b826
Leorio Paradinight <62891774+code-rgb@users.noreply.github.com>
2020-11-21 18:50:52 +0530

    Updated to V0.3.0

--- userge/config.py
+++ userge/config.py
@@ -109,7 +109,7 @@ def get_version() -> str:
         if "/code-rgb/userge-x" in Config.UPSTREAM_REPO.lower():
             diff = list(_REPO.iter_commits(f'v{ver}..HEAD'))
             if diff:
-                return f"{ver}-STORM.{len(diff)}"
+                return f"{ver}-ROGUE.{len(diff)}"
         else:
             diff = list(_REPO.iter_commits(f'{Config.UPSTREAM_REMOTE}/master..HEAD'))
             if diff:
