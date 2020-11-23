11b935e562686b3aca415e4d4f4ebf1c56279ad4
code-rgb <62891774+code-rgb@users.noreply.github.com>
2020-11-23 12:39:52 +0530

    upstreams fixes

--- userge/plugins/misc/download.py
+++ userge/plugins/misc/download.py
@@ -1,31 +1,28 @@
 """ downloader """
 
 
-import asyncio
-import math
+
 import os
-from datetime import datetime
+import math
+import asyncio
 from typing import Tuple, Union
+from datetime import datetime
 from urllib.parse import unquote_plus
 
 from pySmartDL import SmartDL
 
-from userge import Config, Message, userge
-from userge.utils import humanbytes, progress
+from userge import userge, Message, Config
+from userge.utils import progress, humanbytes
 from userge.utils.exceptions import ProcessCanceled
 
 LOGGER = userge.getLogger(__name__)
 
 
-@userge.on_cmd(
-    "download",
-    about={
-        "header": "Download files to server",
-        "usage": "{tr}download [url | reply to telegram media]",
-        "examples": "{tr}download https://speed.hetzner.de/100MB.bin | testing upload.bin",
-    },
-    check_downpath=True,
-)
+@userge.on_cmd("download", about={
+    'header': "Download files to server",
+    'usage': "{tr}download [url | reply to telegram media]",
+    'examples': "{tr}download https://speed.hetzner.de/100MB.bin | testing upload.bin"},
+    check_downpath=True)
 async def down_load_media(message: Message):
     """ download from tg and url """
     if message.reply_to_message and message.reply_to_message.media:
@@ -45,9 +42,7 @@ async def down_load_media(message: Message):
         await message.edit(f"Downloaded to `{dl_loc}` in {d_in} seconds")
 
 
-async def handle_download(
-    message: Message, resource: Union[Message, str]
-) -> Tuple[str, int]:
+async def handle_download(message: Message, resource: Union[Message, str]) -> Tuple[str, int]:
     """ download from resource """
     if isinstance(resource, Message):
         return await tg_download(message, resource)
@@ -72,44 +67,34 @@ async def url_download(message: Message, url: str) -> Tuple[str, int]:
         if message.process_is_canceled:
             downloader.stop()
             raise ProcessCanceled
-        total_length = downloader.filesize or 0
+        total_length = downloader.filesize if downloader.filesize else 0
         downloaded = downloader.get_dl_size()
         percentage = downloader.get_progress() * 100
         speed = downloader.get_speed(human=True)
         estimated_total_time = downloader.get_eta(human=True)
-        progress_str = (
-            "__{}__\n"
-            + "```[{}{}]```\n"
-            + "**Progress** : `{}%`\n"
-            + "**URL** : `{}`\n"
-            + "**FILENAME** : `{}`\n"
-            + "**Completed** : `{}`\n"
-            + "**Total** : `{}`\n"
-            + "**Speed** : `{}`\n"
-            + "**ETA** : `{}`"
-        )
+        progress_str = \
+            "__{}__\n" + \
+            "```[{}{}]```\n" + \
+            "**Progress** : `{}%`\n" + \
+            "**URL** : `{}`\n" + \
+            "**FILENAME** : `{}`\n" + \
+            "**Completed** : `{}`\n" + \
+            "**Total** : `{}`\n" + \
+            "**Speed** : `{}`\n" + \
+            "**ETA** : `{}`"
         progress_str = progress_str.format(
             "trying to download",
-            "".join(
-                (
-                    Config.FINISHED_PROGRESS_STR
-                    for i in range(math.floor(percentage / 5))
-                )
-            ),
-            "".join(
-                (
-                    Config.UNFINISHED_PROGRESS_STR
-                    for i in range(20 - math.floor(percentage / 5))
-                )
-            ),
+            ''.join((Config.FINISHED_PROGRESS_STR
+                     for i in range(math.floor(percentage / 5)))),
+            ''.join((Config.UNFINISHED_PROGRESS_STR
+                     for i in range(20 - math.floor(percentage / 5)))),
             round(percentage, 2),
             url,
             custom_file_name,
             humanbytes(downloaded),
             humanbytes(total_length),
             speed,
-            estimated_total_time,
-        )
+            estimated_total_time)
         count += 1
         if count >= Config.EDIT_SLEEP_TIMEOUT:
             count = 0
@@ -122,11 +107,14 @@ async def tg_download(message: Message, to_download: Message) -> Tuple[str, int]
     """ download from tg file """
     await message.edit("`Downloading From TG...`")
     start_t = datetime.now()
+    custom_file_name = Config.DOWN_PATH
+    if message.filtered_input_str:
+        custom_file_name = os.path.join(Config.DOWN_PATH, message.filtered_input_str.strip())
     dl_loc = await message.client.download_media(
         message=to_download,
-        file_name=Config.DOWN_PATH,
+        file_name=custom_file_name,
         progress=progress,
-        progress_args=(message, "trying to download"),
+        progress_args=(message, "trying to download")
     )
     if message.process_is_canceled:
         raise ProcessCanceled
