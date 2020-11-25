4e06aba96eba7e5f1fad140e9bdec3a4cffcc550
code-rgb <62891774+code-rgb@users.noreply.github.com>
2020-11-25 20:57:51 +0530

    Update warnx.py

--- userge/plugins/admin/warnx.py
+++ userge/plugins/admin/warnx.py
@@ -1,6 +1,6 @@
 from pyrogram.errors import PeerIdInvalid
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-
+from time import time
 from userge import Config, Message, get_collection, userge
 
 WARN_DATA = get_collection("WARN_DATA")
@@ -24,7 +24,7 @@ async def warn_func(message: Message):
     if message.input_str:
         warn_input = message.input_str.split(None, 1)
         reason = message.input_str
-        if len(warn_input) >= 1 and warn_input[0].is_digit():
+        if len(warn_input) >= 1 and warn_input[0].isdigit():
             try:
                 warned_user = await message.client.get_user(warn_input[0])
             except PeerIdInvalid:
@@ -55,13 +55,25 @@ async def warn_func(message: Message):
     )
     chat_title = message.chat.title
     ###
+    banned = False
     if wcount < max_warns:
         wcount += 1
-    warn_text = f"""
+    elif wcount >= max_warns:
+        banned = await message.chat.kick_member(
+            warned_user.id,
+            until_date=int(time() + 60)
+        )
+        
+    if banned:
+        warn_text = f"Warnings has been exceeded! {warned_user.mention} has been banned!"
+    else:
+        warn_text = f"""
 {by_user.mention} has warned {warned_user.mention} in <b>{chat_title}</b>
 Reason: <code>{reason}</code>
 Warns: {wcount}/{max_warns}
 """
+    
+
     warn_id = str(
         (
             await WARNS_DB.insert_one(
@@ -84,18 +96,27 @@ Warns: {wcount}/{max_warns}
         if rules:
             btn_row.append(InlineKeyboardButton("📝  Rules", url=rules))
 
+        buttons = None if banned else InlineKeyboardMarkup([btn_row])
+
         await message.reply(
             warn_text,
             disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup([btn_row]),
+            reply_markup=buttons,
         )
 
+    
 
-@userge.on_cmd("warn mode", about={"header": "warn_mode"})
+
+
+@userge.on_cmd("warnmode", about={"header": "warn_mode"})
 async def warn_mode(message: Message):
+    warn_types = ['kick', 'ban', 'mute']
     warn_mode = message.input_str
+    if not (warn_mode and warn_mode in warn_types):
+        return await message.err('Not a valid warm mode', del_in=5)
+    
     result = await WARN_DATA.update_one(
-        {"chat_id": chat_id}, {"$set": {"warn_mode": warn_mode}}, upsert=True
+        {"chat_id": message.chat.id}, {"$set": {"warn_mode": warn_mode}}, upsert=True
     )
     out = "{} <b>{}</b> for Chat: {}"
     if result.upserted_id:
@@ -109,7 +130,7 @@ async def warn_mode(message: Message):
 async def maxwarns(message: Message):
     maxwarns = message.input_str
     result = await WARN_DATA.update_one(
-        {"chat_id": chat_id}, {"$set": {"rules": int(maxwarns)}}, upsert=True
+        {"chat_id": message.chat.id}, {"$set": {"rules": int(maxwarns)}}, upsert=True
     )
     out = "{} <b>{}</b> for Chat: {}"
     if result.upserted_id:
@@ -123,7 +144,7 @@ async def maxwarns(message: Message):
 async def chat_rules(message: Message):
     rules = message.input_str
     result = await WARN_DATA.update_one(
-        {"chat_id": chat_id}, {"$set": {"rules": rules}}, upsert=True
+        {"chat_id": message.chat.id}, {"$set": {"rules": rules}}, upsert=True
     )
     out = "{} <b>{}</b> for Chat: {}"
     if result.upserted_id:
