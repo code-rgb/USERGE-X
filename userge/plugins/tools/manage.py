4606fe76363db2e0860a8373c7148d01468781f3
Vijay <70903440+Guru-25@users.noreply.github.com>
2020-11-26 09:26:26 +0530

    Update manage.py

--- userge/plugins/tools/manage.py
+++ userge/plugins/tools/manage.py
@@ -36,7 +36,7 @@ async def status(message: Message) -> None:
     name_ = message.filtered_input_str
     type_ = list(message.flags)
     if not type_:
-        out_str = f"""📊 **--Userge Status--** 📊
+        out_str = f"""📊 **--USERGE-X Status--** 📊
 
 🗃 **Plugins** : `{len(userge.manager.plugins)}`
         ✅ **Loaded** : `{len(userge.manager.loaded_plugins)}`
