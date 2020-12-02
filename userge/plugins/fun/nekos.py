68939a8ad74c676a062886ec0b6d87b79babeef9
code-rgb <62891774+code-rgb@users.noreply.github.com>
2020-12-02 20:22:13 +0530

    Fixed Nekos

--- userge/plugins/fun/nekos.py
+++ userge/plugins/fun/nekos.py
@@ -1,22 +1,24 @@
-import os
 
+
+from userge import Message, userge
+import os
+from .nsfw import age_verification
 from anekos import NekosLifeClient, NSFWImageTags, SFWImageTags
 from pyrogram.errors import MediaEmpty
 from wget import download
 
-from userge import Message, userge
-
-from .nsfw import age_verification
-
 client = NekosLifeClient()
+
+NSFW = [x for x in dir(NSFWImageTags) if not x.startswith('__')]
+SFW = [z for z in dir(SFWImageTags) if not z.startswith('__')]
 
 
 neko_help = "<b>NSFW</b> :  "
-for i in NSFWImageTags.to_list():
-    neko_help += f"<code>{i}</code>   "
+for i in NSFW:
+    neko_help += f"<code>{i.lower()}</code>   "
 neko_help += "\n\n<b>SFW</b> :  "
-for m in SFWImageTags.to_list():
-    neko_help += f"<code>{m}</code>   "
+for m in SFW:
+    neko_help += f"<code>{m.lower()}</code>   "
 
 
 @userge.on_cmd(
@@ -35,11 +37,13 @@ async def neko_life(message: Message):
             return
         link = (await client.random_image(nsfw=True)).url
     elif choice:
-        input_choice = choice.strip()
-        if input_choice in SFWImageTags.to_list():
-            link = (await client.image(SFWImageTags[input_choice.upper()])).url
-        elif input_choice in NSFWImageTags.to_list():
-            link = (await client.image(NSFWImageTags[input_choice.upper()])).url
+        input_choice = (choice.strip()).upper()
+        if input_choice in SFW:
+            link = (await client.image(SFWImageTags[input_choice])).url
+        elif input_choice in NSFW:
+            if await age_verification(message):
+                return
+            link = (await client.image(NSFWImageTags[input_choice])).url
         else:
             await message.err(
                 "Choose a valid Input !, See Help for more info.", del_in=5
@@ -49,14 +53,14 @@ async def neko_life(message: Message):
         link = (await client.random_image()).url
 
     await message.delete()
-
+    
     try:
         await send_nekos(message, link)
     except MediaEmpty:
         link = download(link)
         await send_nekos(message, link)
         os.remove(link)
-
+        
 
 async def send_nekos(message: Message, link: str):
     reply = message.reply_to_message
