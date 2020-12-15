186ca197c165495fe3d6d09f0a8fd3e332db54a5
Leorio Paradinight <62891774+code-rgb@users.noreply.github.com>
2020-12-16 00:34:04 +0530

    Update rawbotapi.py

--- userge/utils/rawbotapi.py
+++ userge/utils/rawbotapi.py
@@ -18,33 +18,61 @@ class XBot:
         text: str,
         reply_markup: list = None,
         parse_mode: str = "HTML",
+        disable_web_page_preview: bool = False
     ):
         method = self.api + "/editMessageText"
         params = {
             "inline_message_id": inline_message_id,
             "text": text,
             "parse_mode": parse_mode,
+            "disable_web_page_preview": disable_web_page_preview
         }
-        if reply_markup:
+        if reply_markup: # :: Optional ::
             params["reply_markup"] = json.dumps({"inline_keyboard": reply_markup})
         return await AioHttp.get_json(method, params)
 
     async def editMessageCaption(
         self,
         inline_message_id: str,
-        caption: str,
+        caption: str = None,
         reply_markup: list = None,
         parse_mode: str = "HTML",
     ):
         method = self.api + "/editMessageCaption"
         params = {
             "inline_message_id": inline_message_id,
-            "caption": caption,
             "parse_mode": parse_mode,
         }
-        if reply_markup:
+        if caption: # :: Optional ::
+            params["caption"] = caption
+        if reply_markup: # :: Optional ::
             params["reply_markup"] = json.dumps({"inline_keyboard": reply_markup})
         return await AioHttp.get_json(method, params)
 
+    async def editMessageMedia(
+        self,
+        inline_message_id: str,
+        media: str,
+        reply_markup: list = None,
+    ):
+        method = self.api + "/editMessageMedia"
+        params = {
+            "inline_message_id": inline_message_id,
+            "media": media
+        }
+        if reply_markup: # :: Optional ::
+            params["reply_markup"] = json.dumps({"inline_keyboard": reply_markup})
+        return await AioHttp.get_json(method, params)
 
-# TODO editMessageMedia, editMessageReplyMarkup
+    async def editMessageReplyMarkup(
+        self,
+        inline_message_id: str,
+        reply_markup: list = None,
+    ):
+        method = self.api + "/editMessageReplyMarkup"
+        params = {
+            "inline_message_id": inline_message_id,
+        }
+        if reply_markup: # :: Optional ::
+            params["reply_markup"] = json.dumps({"inline_keyboard": reply_markup})
+        return await AioHttp.get_json(method, params)
