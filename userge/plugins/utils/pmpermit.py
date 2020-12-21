33b0f9eb03cb946d56fe66338231191868c112ec
code-rgb <62891774+code-rgb@users.noreply.github.com>
2020-12-21 11:52:12 +0000

    black autoformat

--- userge/plugins/utils/pmpermit.py
+++ userge/plugins/utils/pmpermit.py
@@ -261,7 +261,8 @@ async def view_current_blockPM_msg(message: Message):
     & ~filters.me
     & ~filters.service
     & ~Config.ALLOWED_CHATS,
-    allow_via_bot=False,group=-1
+    allow_via_bot=False,
+    group=-1,
 )
 async def uninvitedPmHandler(message: Message):
     """ pm message handler """
