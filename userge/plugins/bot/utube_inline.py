ff3181b904e008f0785eb0a9bc823b511c7a1f19
Leorio Paradinight <62891774+code-rgb@users.noreply.github.com>
2020-12-26 01:01:26 +0530

    Update utube_inline.py

--- userge/plugins/bot/utube_inline.py
+++ userge/plugins/bot/utube_inline.py
@@ -3,7 +3,7 @@ from urllib.parse import parse_qs, urlencode, urlparse
 
 import ujson
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-
+from program import filters
 from userge import Message, userge
 from userge.utils import check_owner, get_response, rand_key, xbot, xmedia
 
