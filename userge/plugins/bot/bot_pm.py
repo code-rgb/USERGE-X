68e5c1a9e3002afc0734d34d020f7b903292e7b6
F36 <70635838+F36@users.noreply.github.com>
2021-01-03 09:51:32 +0530

    Update bot_pm.py

--- userge/plugins/bot/bot_pm.py
+++ userge/plugins/bot/bot_pm.py
@@ -46,8 +46,6 @@ if userge.has_bot:
 Hello {f_name},
 Nice To Meet You! I'm **{bot.first_name}** A Bot.
 
-        <i><b>Powered by</i> [USERGE-X](https://t.me/x_xtests)</b>
-
 <b>My Master is: {master.first_name}</b>
 <i>You can contact my <b>Master</b> and checkout the <b>Repo</b> For more info.</i>
 """
@@ -131,10 +129,7 @@ Nice To Meet You! I'm **{bot.first_name}** A Bot.
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
-                            InlineKeyboardButton("CONTACT", url=f"t.me/{u_n}"),
-                            InlineKeyboardButton(
-                                "REPO", url="https://github.com/code-rgb/USERGE-X"
-                            ),
+                            InlineKeyboardButton("CONTACT", url=f"t.me/{u_n}")
                         ],
                         [
                             InlineKeyboardButton(
