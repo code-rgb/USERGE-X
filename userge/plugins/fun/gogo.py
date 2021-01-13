828dd5c0a55e824a558aba35554b60a210a9817d
Leorio Paradinight <62891774+code-rgb@users.noreply.github.com>
2021-01-13 20:01:34 +0530

    Update gogo.py

--- userge/plugins/fun/gogo.py
+++ userge/plugins/fun/gogo.py
@@ -16,8 +16,8 @@ GOGO = "https://gogoanime.so"
 
 class Anime:
     @staticmethod
-    async def _get_html(link: str, down_link: bool = False):
-        if not down_link:
+    async def _get_html(link: str, add_pre: bool = True):
+        if add_pre:
             link = GOGO + link
         content = await get_response.text(link)
         return soup(content, "lxml")
@@ -47,45 +47,27 @@ class Anime:
 
     @staticmethod
     async def get_eps(link: str):
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.binary_location = Config.GOOGLE_CHROME_BIN
-        chrome_options.add_argument("--ignore-certificate-errors")
-        chrome_options.add_argument("--test-type")
-        chrome_options.add_argument("--headless")
-        chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument("--disable-dev-shm-usage")
-        chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument("--disable-gpu")
-        driver = webdriver.Chrome(chrome_options=chrome_options)
-        driver.get(link)
-        html = driver.find_element_by_xpath('//*[@id="episode_related"]').get_attribute(
-            "innerHTML"
-        )
-        driver.close()
-        page = soup(html, "lxml")
-        out = {}
-        for i in page.findAll("a"):
-            ep_ = i.div.text.replace("EP", "").strip()
-            if ep_.isdigit():
-                out[int(ep_)] = i.get("href").strip()
-        return out
+        page = await Anime._get_html(link, add_pre=False)
+        end_ = int(page.find("ul", {"id": "episode_page"}).findAll("li")[-1].a.get("ep_end")))
+        name_ = "/" + (link.rsplit("/", 1))[1]
+        return {'total': end_, "name": name_}
 
     @staticmethod
-    async def get_quality(endpoint: str):
+    async def get_quality(anime_: str, episode):
+        endpoint = f"{anime_}-episode-{episode}"
         page_ = await Anime._get_html(endpoint)
         link_ = page_.find("li", {"class": "dowloads"}).a.get("href")
         # get qualities from download page
-        page = await Anime._get_html(link_, down_link=True)
+        page = await Anime._get_html(link_, add_pre=False)
         btn_, row_ = [], []
         for i in page.findAll("div", {"class": "dowload"}):
             qual = i.a
             if qual.get("target") != "_blank":
                 name = qual.text.replace("Download", "").strip()
-                if "HDP" not in name:
-                    btn_.append(InlineKeyboardButton(name, url=qual.get("href")))
-                    if len(btn_) == 2:
-                        row_.append(btn_)
-                        btn_ = []
+                btn_.append(InlineKeyboardButton(name, url=qual.get("href")))
+                if len(btn_) == 2:
+                    row_.append(btn_)
+                    btn_ = []
         if len(btn_) != 0:
             row_.append(btn)
         return InlineKeyboardMarkup(row_)
