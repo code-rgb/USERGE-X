b0e90b705751533fc987bf63736908065a29d1d8
Leorio Paradinight <62891774+code-rgb@users.noreply.github.com>
2021-01-15 12:49:10 +0530

    Added Anime Download Plugin (#105)
    * Create gogo.py
    
    * fixed minor bug in quote
    
    * Update utube_inline.py
    
    * Fixed deepsouce

---
+++ userge/plugins/bot/gogo.py
@@ -0,0 +1,232 @@
+"""Download Anime"""
+
+# For USERGE-X
+# (C) 2021 All Rights Reserved
+# Idea by: [TG: @Lostb053]
+# Author: (github.com/code-rgb) [TG: @DeletedUser420]
+
+
+import asyncio
+from urllib.parse import quote
+
+from bs4 import BeautifulSoup as soup
+from pyrogram import filters
+from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
+
+from userge import Config, Message, userge
+from userge.utils import check_owner, get_response, rand_key
+
+GOGO = "https://gogoanime.so"
+GOGO_DB = {}
+
+
+@userge.on_cmd("gogo", about={"header": "Gogo Plugin Help"})
+async def gogo_h(message: Message):
+    if not userge.has_bot:
+        await message.err("You Need to create a bot via Bot Father", del_in=5)
+        return
+    link_ = "https://t.me/{}?start=inline".format((await userge.bot.get_me()).username)
+    if message.client.is_bot:
+        await userge.bot.send_message(
+            message.chat.id,
+            text="To know how to use this plugin,\nClick the button below",
+            reply_markup=InlineKeyboardMarkup(
+                [[InlineKeyboardButton("See Help for Anime", url=link_)]]
+            ),
+        )
+        await message.delete()
+    else:
+        await message.edit(
+            "[**See Help for Anime**]({})".format(link_), disable_web_page_preview=True
+        )
+
+
+class Anime:
+    @staticmethod
+    async def _get_html(link: str, add_pre: bool = True):
+        if add_pre:
+            link = GOGO + link
+        content = await get_response.text(link)
+        return soup(content, "lxml")
+
+    @staticmethod
+    async def search(query: str):
+        page = await Anime._get_html("/search.html?keyword=" + quote(query))
+        out = []
+        for i in page.find("ul", {"class": "items"}).findAll("li"):
+            result_ = i.find("p", {"class": "name"})
+            title = result_.a.text.strip()
+            release = i.find("p", {"class": "released"}).text.strip()
+            result_url = GOGO + result_.a.get("href")
+            image = i.div.a.img.get("src")
+            k_ = image.rsplit("/", 1)
+            if len(k_) == 2:
+                k_[1] = quote(k_[1])
+            key_ = rand_key()
+            body_ = f"[\u200c]({'/'.join(k_)})**{title}**\n{release}"
+            GOGO_DB[key_] = {"url": result_url, "body": body_}
+            out.append(
+                {
+                    "key": key_,
+                    "title": title,
+                    "release": release,
+                    "image": "/".join(k_),
+                }
+            )
+        return out
+
+    @staticmethod
+    async def get_eps(link: str):
+        page = await Anime._get_html(link, add_pre=False)
+        end_ = page.find("ul", {"id": "episode_page"}).findAll("li")[-1].a.get("ep_end")
+        return end_
+
+    @staticmethod
+    def _get_name(link: str):
+        name_ = "/" + (link.rsplit("/", 1))[1]
+        return name_
+
+    @staticmethod
+    async def get_quality(url: str, episode: int, key_: str):
+        endpoint = f"{Anime._get_name(url)}-episode-{episode}"
+        page_ = await Anime._get_html(endpoint)
+        link_ = page_.find("li", {"class": "dowloads"}).a.get("href")
+        # get qualities from download page
+        page = await Anime._get_html(link_, add_pre=False)
+        btn_, row_ = [], []
+        for i in page.findAll("div", {"class": "dowload"}):
+            qual = i.a
+            if qual.get("target") != "_blank":
+                name = qual.text.replace("Download", "").strip()
+                btn_.append(InlineKeyboardButton(name, url=qual.get("href")))
+                if len(btn_) == 2:
+                    row_.append(btn_)
+                    btn_ = []
+        if len(btn_) != 0:
+            row_.append(btn_)
+        row_.append(
+            [InlineKeyboardButton("Back", callback_data=f"get_currentpg{key_}")]
+        )
+        return InlineKeyboardMarkup(row_)
+
+
+if userge.has_bot:
+
+    @userge.bot.on_callback_query(filters.regex(pattern="get_eps(.*)"))
+    @check_owner
+    async def get_eps_from_key(c_q: CallbackQuery):
+        key_ = c_q.matches[0].group(1)
+        url_ = GOGO_DB.get(key_)
+        if not url_:
+            return await c_q.answer("Not Found")
+        url_ = url_.get("url")
+        await c_q.answer()
+        res = await Anime.get_eps(url_)
+        btn_, row_, paginate = [], [], []
+        for i in range(1, int(res) + 1):
+            btn_.append(
+                InlineKeyboardButton(
+                    "EP " + str(i), callback_data=f"gogo_get_qual{key_}_{i}"
+                )
+            )
+            if len(btn_) == 4:
+                row_.append(btn_)
+                btn_ = []
+            if len(row_) == 7:
+                paginate.append(row_)
+                row_ = []
+        if len(btn_) != 0:
+            row_.append(btn_)
+        if len(row_) != 0:
+            paginate.append(row_)
+        GOGO_DB[key_]["page"] = paginate
+        p_len = len(paginate)
+        if p_len > 1:
+            paginate[0].append(
+                [
+                    InlineKeyboardButton(
+                        "1 / " + str(p_len), callback_data=f"gogo_page{key_}_0"
+                    ),
+                    InlineKeyboardButton("Next", callback_data=f"gogo_next{key_}_0"),
+                ]
+            )
+        await c_q.edit_message_reply_markup(
+            reply_markup=InlineKeyboardMarkup(paginate[0])
+        )
+
+    @userge.bot.on_callback_query(
+        filters.regex(pattern=r"gogo_get_qual([a-z0-9]+)_([\d]+)")
+    )
+    @check_owner
+    async def get_qual_from_eps(c_q: CallbackQuery):
+        key_ = c_q.matches[0].group(1)
+        episode = int(c_q.matches[0].group(2))
+        url_ = GOGO_DB.get(key_)
+        if not url_:
+            return await c_q.answer("Not Found")
+        url_ = url_.get("url")
+        await c_q.answer()
+        await c_q.edit_message_text(
+            text=f"{url_.get('body')}\n• **Episode: {episode}**\n\n📹  Choose the desired quality from below\n**Note: **for uploading to TG:\n>>  `{Config.CMD_TRIGGER}upload [link] | [filename].mp4`\ne.g  {Config.CMD_TRIGGER}upload [link] | video.mp4",
+            reply_markup=(
+                await Anime.get_quality(url=url_, episode=episode, key_=key_)
+            ),
+        )
+
+    @userge.bot.on_callback_query(
+        filters.regex(pattern=r"gogo_(page|next|back)([a-z0-9]+)_([\d]+)")
+    )
+    @check_owner
+    async def gogo_paginate(c_q: CallbackQuery):
+        direction = c_q.matches[0].group(1)
+        key_ = c_q.matches[0].group(2)
+        pos = int(c_q.matches[0].group(3))
+        key_data = GOGO_DB.get(key_)
+        if not key_data:
+            return await c_q.answer("Not Found")
+        await c_q.answer()
+        pages = key_data.get("page")
+        p_len = len(pages)
+        del_back = False
+        if direction == "next":
+            page = pos + 1
+            if page >= p_len:
+                return await c_q.answer("That's All Folks !")
+        elif direction == "back":
+            del_back = pos == 1
+            page = pos - 1
+        else:
+            return
+        button_base = [
+            InlineKeyboardButton("Back", callback_data=f"gogo_back{key_}_{page}"),
+            InlineKeyboardButton(
+                f"{page + 1} / {p_len}",
+                callback_data=f"gogo_page{key_}_{page}",
+            ),
+            InlineKeyboardButton("Next", callback_data=f"gogo_next{key_}_{page}"),
+        ]
+        if del_back:
+            button_base.pop(0)
+        # Work Around for multiple nav buttons
+        # idk why "pages" is acting as a global variable
+        if pages[page][-1][-1].text == "Next":
+            pages[page][-1] = button_base
+        else:
+            pages[page].append(button_base)
+        GOGO_DB[key_]["current_pg"] = pages[page]
+        await c_q.edit_message_reply_markup(
+            reply_markup=InlineKeyboardMarkup(pages[page])
+        )
+        await asyncio.sleep(0.3)
+
+    @userge.bot.on_callback_query(filters.regex(pattern=r"get_currentpg(.*)"))
+    @check_owner
+    async def get_current_pg(c_q: CallbackQuery):
+        key_ = c_q.matches[0].group(1)
+        data_ = GOGO_DB.get(key_)
+        if not data_:
+            return await c_q.answer("Not Found")
+        await c_q.answer()
+        mrkp = data_.get("current_pg")
+        body_ = data_.get("body")
+        await c_q.edit_message_text(text=body_, reply_markup=InlineKeyboardMarkup(mrkp))
--- userge/plugins/bot/inline_help.py
+++ userge/plugins/bot/inline_help.py
@@ -17,39 +17,39 @@ COMMANDS = {
         "i_q": "secret @DeletedUser420 This is a secret message",
     },
     "alive": {
-        "help_txt": "**Alive Command for USERGE-X**\n\nHere You can view Uptime, Setting and Versions of your bot and when you change settings they are updated in Real-time UwU\n>>>  `alive`",
+        "help_txt": "**Alive Command for USERGE-X**\nHere You can view Uptime, Setting and Versions of your bot and when you change settings they are updated in Real-time UwU\n\n>>>  `alive`",
         "i_q": "alive",
     },
     "opinion": {
-        "help_txt": "**Ask for opinion via inline**\n\nYou can now send multiple opinion messages at once\n**Note: **All button data is cleared as soon as you restart or update the bot\n>>>  `op [Question or Statement]`",
+        "help_txt": "**Ask for opinion via inline**\nYou can now send multiple opinion messages at once\n**Note: **All button data is cleared as soon as you restart or update the bot\n\n>>>  `op [Question or Statement]`",
         "i_q": "op Are Cats Cute ?",
     },
     "repo": {
-        "help_txt": "**Your USERGE-X Github repo**\n\nwith direct deploy button\n>>>  `repo`",
+        "help_txt": "**Your USERGE-X Github repo**\nwith direct deploy button\n\n>>>  `repo`",
         "i_q": "repo",
     },
     "gapps": {
-        "help_txt": "**Lastest arm64 Gapps for <u>Android 10 Only !</u>**\n\nChoose from Niksgapps, Opengapps and Flamegapps\n>>>  `gapps`",
+        "help_txt": "**Lastest arm64 Gapps for <u>Android 10 Only !</u>**\nChoose from Niksgapps, Opengapps and Flamegapps\n\n>>>  `gapps`",
         "i_q": "gapps",
     },
     "ofox": {
-        "help_txt": "**Lastest Ofox Recovery for supported device, Powered By offcial Ofox API v2**\n\n>>>  `ofox <device codename>`",
+        "help_txt": "**Lastest Ofox Recovery for supported device, Powered By offcial Ofox API v2**\n\n>>>  `ofox [device codename]`",
         "i_q": "ofox whyred",
     },
     "reddit": {
-        "help_txt": '**Get Reddit Image post**\n\nGet Random Reddit meme or a post from specific subreddit, if you want post from specific subreddit do "reddit [subreddit]."\n>>> `reddit  or  reddit dankmemes.`',
+        "help_txt": '**Get Reddit Image post**\nGet Random Reddit meme or a post from specific subreddit, if you want post from specific subreddit do "reddit [subreddit]."\n\n>>> `reddit  or  reddit dankmemes.`',
         "i_q": "reddit nextfuckinglevel.",
     },
     "help": {
-        "help_txt": "**Help For All Userbot plugins**\n\n**Note:** `You can also load and unload a plugin, and which chat types the commands is permitted`",
+        "help_txt": "**Help For All Userbot plugins**\n**Note:** `You can also load and unload a plugin, and which chat types the commands is permitted`",
         "i_q": "",
     },
     "stylish": {
-        "help_txt": "**Write it in Style**\n\nplugin to decorate text with unicode fonts.\n>>>  `stylish [text]`",
+        "help_txt": "**Write it in Style**\nplugin to decorate text with unicode fonts.\n\n>>>  `stylish [text]`",
         "i_q": "stylish USERGE-X",
     },
     "ytdl": {
-        "help_txt": f"**Download YouTube Videos or Audio with Buttons**\n\nTo Download Video / Audio from youtube with desired quality.\n>>>  `ytdl [URL or Text]` or `{Config.CMD_TRIGGER}iytdl` iytdl [URL / Text] or [Reply to URL / Text]",
+        "help_txt": f"**Download YouTube Videos or Audio with Buttons**\nTo Download Video / Audio from youtube with desired quality.\n\n>>>  `ytdl [URL or Text]\n   Non-Inline: {Config.CMD_TRIGGER}iytdl [URL / Text] or [Reply to URL / Text]`",
         "i_q": "ytdl https://www.youtube.com/watch?v=dQw4w9WgXcQ",
     },
     "spoiler": {
@@ -59,6 +59,10 @@ COMMANDS = {
     "btn": {
         "help_txt": "**Get upto 15 of your most recently created inline messages in the inline query, so you can post it in any channel or group effortlessly**\n For Creating inline messages see `.help .ibutton`\n\n>>>  `btn`",
         "i_q": "btn",
+    },
+    "anime": {
+        "help_txt": "**Anime Downloader**\nSearch Anime via inline bot and then choose episodes number and desired quality\n\n>>> `anime [Query]`",
+        "i_q": "anime Naruto",
     },
 }
 
@@ -82,14 +86,14 @@ if userge.has_bot:
     if not HELP_BUTTONS:
         HELP_BUTTONS = help_btn_generator()
 
-    BACK_BTN = InlineKeyboardButton(" ◀️  Back ", callback_data="backbtn_ihelp")
+    BACK_BTN = InlineKeyboardButton("◀️  Back", callback_data="backbtn_ihelp")
 
-    inline_help_txt = " <u><b>INLINE COMMANDS</b></u>\n\nHere is a list of all available inline commands.\nChoose a command and for usage see:\n[ **📕  EXAMPLE** ]"
+    inline_help_txt = " <u><b>INLINE COMMANDS</b></u>\n\nHere is a list of all available inline commands.\nChoose a command and for usage see:\n**📕  EXAMPLE**"
 
     @userge.bot.on_message(
         filters.user(list(Config.OWNER_ID))
         & filters.private
-        & (filters.command("inline") | filters.regex(pattern=r"^/start inline$"))
+        & filters.command("inline")
     )
     async def inline_help(_, message: Message):
         await userge.bot.send_message(
@@ -102,6 +106,7 @@ if userge.has_bot:
         filters.user(list(Config.OWNER_ID)) & filters.regex(pattern=r"^backbtn_ihelp$")
     )
     async def back_btn(_, c_q: CallbackQuery):
+        await c_q.answer()
         await c_q.edit_message_text(
             text=inline_help_txt, reply_markup=InlineKeyboardMarkup(HELP_BUTTONS)
         )
@@ -111,6 +116,7 @@ if userge.has_bot:
         & filters.regex(pattern=r"^ihelp_([a-zA-Z]+)$")
     )
     async def help_query(_, c_q: CallbackQuery):
+        await c_q.answer()
         command_name = c_q.matches[0].group(1)
         msg = COMMANDS[command_name]["help_txt"]
         switch_i_q = COMMANDS[command_name]["i_q"]
@@ -118,7 +124,7 @@ if userge.has_bot:
             [
                 BACK_BTN,
                 InlineKeyboardButton(
-                    " 📕  EXAMPLE ", switch_inline_query_current_chat=switch_i_q
+                    "📕  EXAMPLE", switch_inline_query_current_chat=switch_i_q
                 ),
             ]
         ]
