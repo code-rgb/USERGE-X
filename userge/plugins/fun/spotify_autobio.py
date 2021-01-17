d6ed149e9c16c5cc8256577204ced649a1a94ea5
Leorio Paradinight <62891774+code-rgb@users.noreply.github.com>
2021-01-17 11:10:29 +0530

    Update spotify_autobio.py

--- userge/plugins/fun/spotify_autobio.py
+++ userge/plugins/fun/spotify_autobio.py
@@ -587,13 +587,5 @@ async def sp_recents_(message: Message):
         get_name = track["name"]
         ex_link = track["external_urls"]
         get_link = ex_link["spotify"]
-        recent += (
-            "• ["
-            + escape_markdown(get_name)
-            + "]"
-            + "("
-            + escape_markdown(get_link)
-            + ")"
-            + "\n"
-        )
+        recent += "• [{}]({})\n".format(escape_markdown(get_name), escape_markdown(get_link))
     await message.edit(recent, disable_web_page_preview=True)
