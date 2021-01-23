491973389a26f0075c52cd5324ca6315ee4b4de9
Alícia <61088084+Thegreatfoxxgoddess@users.noreply.github.com>
2021-01-24 03:30:31 +0530

    Update alive.py

--- userge/plugins/bot/alive.py
+++ userge/plugins/bot/alive.py
@@ -130,7 +130,7 @@ class Bot_Alive:
 <b>[Paimon](tg://openmessage?user_id=1486647366) is Up and Running....
 
   Python</b> :              🐍  <code>v{versions.__python_version__}</code>
-  <b>Pyrogram</b> :        🔥  <code>v{versions.__pyro_version__}</code>-X-158
+  <b>Pyrogram</b> :          🔥  <code>v{versions.__pyro_version__}</code>-X-158
   Bot Version :       🧬  <code>v{get_version()}</code>
   Maintainer :       🦋 [Alícia Dark](tg://openmessage?user_id=1360435532)
   
