a7e616f5f5de2e58e723ac568b45384e63dfa6dc
Alícia <61088084+Thegreatfoxxgoddess@users.noreply.github.com>
2021-01-23 09:07:35 -0300

    Update alive.py

--- userge/plugins/bot/alive.py
+++ userge/plugins/bot/alive.py
@@ -129,11 +129,12 @@ class Bot_Alive:
         alive_info = f"""
 <b>[Paimon](tg://openmessage?user_id=1486647366) is Up and Running....
 
-  Python</b> :                 🐍  <code>v{versions.__python_version__}</code>
-  <b>Pyrogram</b> :         🔥  <code>v{versions.__pyro_version__}</code>-X-158
-  Bot Version :            🧬  <code>v{get_version()}</code>
-  Maintainer :         🦋 [Alícia Dark](tg://openmessage?user_id=1360435532)
-  <b>Bot Mode    {Bot_Alive._get_mode()}</b>   |   {userge.uptime}
+  Python</b> :              🐍  <code>v{versions.__python_version__}</code>
+  <b>Pyrogram</b> :       🔥  <code>v{versions.__pyro_version__}</code>-X-158
+  Bot Version :        🧬  <code>v{get_version()}</code>
+  Maintainer :       🦋 [Alícia Dark](tg://openmessage?user_id=1360435532)
+  
+  <b>Bot Mode  :  {Bot_Alive._get_mode()}</b>   |   {userge.uptime}
 """
         return alive_info
 
