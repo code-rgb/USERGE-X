c94815aeb11895bca162cb47c3270ac27dfab74e
code-rgb <62891774+code-rgb@users.noreply.github.com>
2021-02-08 16:40:02 +0000

    black autoformat

--- userge/plugins/bot/opinion.py
+++ userge/plugins/bot/opinion.py
@@ -2,13 +2,14 @@
 # All rights reserved.
 
 
+import asyncio
 import os
 
 import ujson
 from pyrogram import filters
 from pyrogram.errors import BadRequest, FloodWait
 from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
-import asyncio
+
 from userge import Config, Message, userge
 
 if not os.path.exists("userge/xcache"):
