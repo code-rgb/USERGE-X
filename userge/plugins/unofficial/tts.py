51b70f9bc3c708500b8ab2205dd1835d5b232d27
shahrooz M <sibche35@gmail.com>
2021-02-09 10:17:51 +0330

    tts bug fix

--- userge/plugins/unofficial/tts.py
+++ userge/plugins/unofficial/tts.py
@@ -12,31 +12,22 @@ from userge import Message, userge
 async def text_to_speech(message: Message):
     req_file_name = "gtts.mp3"
     inp_text = message.input_str
-    if ("|" not in inp_text) or not inp_text:
+    if not inp_text:
         await message.edit("Pathetic")
         return
-    await message.edit("Processing.")
     def_lang = "en"
-    def_lang, text = inp_text.split("|")
+    text = inp_text
     try:
-        await message.edit("Processing..")
         speeched = gTTS(text, lang=def_lang.strip())
         speeched.save(req_file_name)
-        await message.edit("Processing...")
         meta = XMan(CPR(req_file_name))
         a_len = 0
-        a_title = "Text To Speech"
-        a_perf = "Google"
-        a_cap = f"Language Code: {def_lang}"
         if meta and meta.has("duration"):
             a_len = meta.get("duration").seconds
         await message.edit("Uploading...")
         await message.reply_audio(
             audio=req_file_name,
-            caption=a_cap,
-            duration=a_len,
-            performer=a_perf,
-            title=a_title,
+            duration=a_len
         )
         os.remove(req_file_name)
         await message.delete()
