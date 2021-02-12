249f27f68ed446fdcd6fa87f6c3db3d1a74a6d6f
shahrooz M <sibche35@gmail.com>
2021-02-12 12:59:33 +0330

    bug fix

--- userge/plugins/misc/trainbit_uploader.py
+++ userge/plugins/misc/trainbit_uploader.py
@@ -5,7 +5,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from webdriver_manager.chrome import ChromeDriverManager
 
 from userge import Config, Message, userge
 from pudb import set_trace
