8bd374216231cf9179aa77a80798b3458e5fcfa4
Leorio Paradinight <62891774+code-rgb@users.noreply.github.com>
2021-02-14 00:03:00 +0530

    Testing
    Co-Authored-By: sandeep.n <58665444+sandy1709@users.noreply.github.com>

---
+++ userge/plugins/misc/google_img.py
@@ -0,0 +1,28 @@
+from userge.utils.google_images import googleimagesdownload
+from userge import Message, userge
+
+@userge.on_cmd(
+    "gimgs",
+    about={
+        "header": "get a random reddit post ",
+        "usage": "{tr}reddit [subreddit]",
+        "examples": "{tr}reddit dankmemes",
+    },
+)
+async def aaa(message: Message):
+    """Random reddit post"""
+
+    response = googleimagesdownload()
+    # creating list of arguments
+    limit = min(int(message.flags.get("l"), 3), 100)
+    limit = limit if limit > 0 else 3
+    arguments = {
+        "keywords": message.filtered_inp_str,
+        "limit": limit,
+        "format": "jpg",
+        "no_directory": "no_directory",
+    }
+    # passing the arguments to the function
+
+    paths = response.download(arguments)
+    print(paths)
