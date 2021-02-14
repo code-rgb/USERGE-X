6b7e84c68218bf21d93074a63c2ed82906a63ad9
Leorio Paradinight <62891774+code-rgb@users.noreply.github.com>
2021-02-14 21:42:00 +0530

    Ok

--- userge/config.py
+++ userge/config.py
@@ -115,7 +115,7 @@ def get_version() -> str:
             if diff:
                 return f"{ver}-LOGAN.{len(diff)}"
         else:
-            diff = list(_REPO.iter_commits(f'{Config.UPSTREAM_REMOTE}/master..HEAD'))
+            diff = list(_REPO.iter_commits(f'{Config.UPSTREAM_REMOTE}/alpha..HEAD'))
             if diff:
                 return f"{ver}-fork-[X].{len(diff)}"
     except Exception as e:
