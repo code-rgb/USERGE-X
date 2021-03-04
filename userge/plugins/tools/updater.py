2c8935e0e06c724a58cfe9dbdac3c79bf733cb62
Leorio Paradinight <62891774+code-rgb@users.noreply.github.com>
2021-03-04 17:40:56 +0530

    Update updater.py

--- userge/plugins/tools/updater.py
+++ userge/plugins/tools/updater.py
@@ -17,13 +17,11 @@ CHANNEL = userge.getCLogger(__name__)
         "header": "Check Updates or Update USERGE-X",
         "flags": {
             "-pull": "pull updates",
-            #"-push": "push updates to heroku",
             "-branch": "Default is -alpha",
         },
-        "usage": "{tr}update : check updates from default branch\n"
+        "usage": ("{tr}update : check updates from default branch\n"
         "{tr}update -[branch_name] : check updates from any branch\n"
-        "add -pull if you want to pull updates\n",
-        #"add -push if you want to push updates to heroku",
+        "add -pull if you want to pull updates\n"),
         "examples": "{tr}update -pull"
     },
     del_pre=True,
