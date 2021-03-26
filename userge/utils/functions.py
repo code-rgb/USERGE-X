ec38b784f9c064a583dfb54ff3852800aa79c6b3
bhoomi-s-007 <71869549+bhoomi-s-007@users.noreply.github.com>
2021-03-26 14:33:06 +0530

    Update functions.py

--- userge/utils/functions.py
+++ userge/utils/functions.py
@@ -156,7 +156,7 @@ def check_owner(func):
                 pass
         else:
             await c_q.answer(
-                "Only My Master can Access This !!\n\n     𝘿𝙚𝙥𝙡𝙤𝙮 𝙮𝙤𝙪𝙧 𝙤𝙬𝙣 𝙐𝙎𝙀𝙍𝙂𝙀-𝙓",
+                "Only My Master can Access This !!\n\nNub😒😒",
                 show_alert=True,
             )
 
